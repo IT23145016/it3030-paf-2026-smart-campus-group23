0a142a24c271d57e222082f174b835df3ccb5976
IT23145016 <ayodhyamitho@gmail.com>
2026-04-09 12:54:55 +0530

    Fixed git error related to verify()

--- backend/src/test/java/com/scoh/api/service/BookingServiceTest.java
+++ backend/src/test/java/com/scoh/api/service/BookingServiceTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.scoh.api.domain.Booking;
@@ -22,7 +23,6 @@ import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
